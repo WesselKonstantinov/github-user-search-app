29d8890a64b20c65abc3e3e96fb14db56cc68ad6
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-10-17 19:36:58 +0200

    Customize color palette and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      blue: "hsl(212 100% 50%)",
+      "searching-blue": "hsl(217 20% 51%)",
+      "san-marino": "hsl(217 35% 45%)",
+      "anchors-aweigh": "hsl(217 21% 21%)",
+      "ghost-white": "hsl(227 100% 98%)",
+      white: "hsl(0 0% 100%)",
+      "hei-se-black": "hsl(220 40% 13%)",
+      "fainting-light": "hsl(222 41% 20%)",
+    },
+    fontFamily: {
+      mono: ["Space Mono", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
