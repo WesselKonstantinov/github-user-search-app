9535c2f9d7adace310dba83ce0fe79afa10dbb1c
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-10-18 20:01:21 +0200

    Create theme switcher component

---
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -0,0 +1,16 @@
+function ThemeSwitcher() {
+  return (
+    <button className="inline-flex gap-x-4 font-bold tracking-[0.19em] uppercase">
+      Dark
+      <img
+        src="/images/icon-moon.svg"
+        alt=""
+        width="20"
+        height="20"
+        className="w-5 h-5"
+      />
+    </button>
+  );
+}
+
+export default ThemeSwitcher;
