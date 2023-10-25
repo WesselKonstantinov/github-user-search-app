c0d1cd2671cc2e7d6ae5f62971b490f66e091819
Wessel Konstantinov <wes_kon1@hotmail.com>
2023-10-25 19:47:27 +0200

    Implement manual selection

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,14 +1,64 @@
+import { useState, useEffect } from "react";
+
 function ThemeSwitcher() {
+  const [theme, setTheme] = useState("light");
+
+  const prefersDark = theme === "dark";
+
+  const setDarkTheme = () => {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+    setTheme("dark");
+  };
+
+  const setLightTheme = () => {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+    setTheme("light");
+  };
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      setTheme("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      setTheme("light");
+    }
+  }, []);
+
   return (
-    <button className="inline-flex gap-x-4 text-san-marino dark:text-white font-bold tracking-[0.19em] uppercase">
-      Dark
-      <img
-        src="/images/icon-moon.svg"
-        alt=""
-        width="20"
-        height="20"
-        className="w-5 h-5"
-      />
+    <button
+      className="inline-flex gap-x-4 text-san-marino dark:text-white font-bold tracking-[0.19em] uppercase"
+      onClick={prefersDark ? setLightTheme : setDarkTheme}
+    >
+      {prefersDark ? (
+        <>
+          Light
+          <img
+            src="/images/icon-sun.svg"
+            alt=""
+            width="20"
+            height="20"
+            className="w-5 h-5"
+          />
+        </>
+      ) : (
+        <>
+          Dark
+          <img
+            src="/images/icon-moon.svg"
+            alt=""
+            width="20"
+            height="20"
+            className="w-5 h-5"
+          />
+        </>
+      )}
     </button>
   );
 }
