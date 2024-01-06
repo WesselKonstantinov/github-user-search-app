4d9eb27bd95ebdec354e851fa1ec4a3fb0015c90
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-06 22:11:27 +0100

    Add error handling

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,17 +1,31 @@
 import useSWR from "swr";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 function useUser(username) {
   const { data, error, isLoading } = useSWR(
     `https://api.github.com/users/${username}`,
-    fetcher
+    fetcher,
+    {
+      errorRetryCount: 0,
+      revalidateOnFocus: false,
+    }
   );
 
   return {
     user: data,
     isLoading,
-    isError: error,
+    error,
   };
 }
 
