4ad11634e38a857722c1ac74ea1d49e115fc0938
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-10 22:40:17 +0100

    Add hidden live region for dynamic updates

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -4,22 +4,24 @@ import UserIntro from "../UserIntro/UserIntro";
 import UserStats from "../UserStats/UserStats";
 
 function UserProfile({ username }) {
-  const { user, error, isLoading } = useUser(username);
+  const { user, error } = useUser(username);
   const rateLimitExceeded = error?.status === 403 || error?.status === 429;
   const isServerError = error?.status >= 500;
 
   return (
     <>
-      {isLoading && <p className="mt-4">Loading...</p>}
       {rateLimitExceeded && (
-        <p className="mt-4">
+        <p>
           You have exceeded the number of allowed requests. Please try again
           later.
         </p>
       )}
-      {isServerError && <p className="mt-4">Failed to load.</p>}
+      {isServerError && <p>Failed to load.</p>}
       {user && (
-        <div className="pt-8 px-6 pb-12 md:p-10 lg:p-12 mt-4 md:mt-6 grid grid-cols-2 lg:grid-cols-[7.3125rem_1fr] gap-6 md:gap-8 lg:gap-x-9 bg-white dark:bg-fainting-light rounded-2xl shadow-[0_1rem_1.875rem_-0.625rem_rgba(70,96,187,0.2)] dark:shadow-none">
+        <div className="pt-8 px-6 pb-12 md:p-10 lg:p-12 grid grid-cols-2 lg:grid-cols-[7.3125rem_1fr] gap-6 md:gap-8 lg:gap-x-9 bg-white dark:bg-fainting-light rounded-2xl shadow-[0_1rem_1.875rem_-0.625rem_rgba(70,96,187,0.2)] dark:shadow-none">
+          <div aria-live="polite" aria-atomic="true" className="sr-only">
+            {user && <p>Results updated to user {user.login}</p>}
+          </div>
           <UserIntro
             name={user.name}
             login={user.login}
