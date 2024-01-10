a48a3edd4f1523babce41b307e3074861b15fcf9
Wessel Konstantinov <wes_kon1@hotmail.com>
2024-01-10 22:40:51 +0100

    Fix layout

--- src/components/AppMain/AppMain.jsx
+++ src/components/AppMain/AppMain.jsx
@@ -6,7 +6,7 @@ function AppMain({ title }) {
   const [username, setUsername] = useState("octocat");
 
   return (
-    <main className="mt-9">
+    <main className="mt-9 flex flex-col gap-4 md:gap-6">
       <h1 className="sr-only">{title}</h1>
       <SearchBar username={username} setUsername={setUsername} />
       <UserProfile username={username} />
